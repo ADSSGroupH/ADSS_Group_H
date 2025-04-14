c138d34c38d7a2c87c621372977d5fb1fa1b4d6a
hilapelet <hilapele@post.bgu.ac.il>
2025-04-14 17:00:50 +0300

    Update HRManager.java

--- dev/partA/HRManager.java
+++ dev/partA/HRManager.java
@@ -1,2 +1,9 @@
-public class HRManager {
+import java.util.Set;
+
+public class HRManager extends Employee {
+    public HRManager(String id, String name, String phoneNumber, Branch branch,
+                     Set<Role> roles, EmployeeContract contract, String bankDetails,
+                     boolean isArchived, String archivedAt) {
+        super(id, name, phoneNumber, branch, roles, contract, bankDetails, isArchived, archivedAt);
+    }
 }
