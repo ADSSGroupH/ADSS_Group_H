4b4aa33795727a614ada88759ffbdde89f07fa18
hilapelet <hilapele@post.bgu.ac.il>
2025-04-14 19:13:02 +0300

    Update Employee.java

--- dev/partA/Employee.java
+++ dev/partA/Employee.java
@@ -24,4 +24,68 @@ public class Employee {
         this.isArchived = isArchived;
         this.archivedAt = archivedAt;
     }
+
+    // --- Getters and Setters ---
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public EmployeeContract getContract() {
+        return contract;
+    }
+    public void setContract(EmployeeContract contract) {
+        this.contract = contract;
+    }
+
+    public String getBankDetails() {
+        return bankDetails;
+    }
+    public void setBankDetails(String bankDetails) {
+        this.bankDetails = bankDetails;
+    }
+
+    public boolean isArchived() {
+        return isArchived;
+    }
+    public void setArchived(boolean archived) {
+        isArchived = archived;
+    }
+
+    public String getArchivedAt() {
+        return archivedAt;
+    }
+    public void setArchivedAt(String archivedAt) {
+        this.archivedAt = archivedAt;
+    }
 }
