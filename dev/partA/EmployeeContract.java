8318568f0ea0c85accf87261d3291a0fd1bd6ac3
hilapelet <hilapele@post.bgu.ac.il>
2025-04-14 17:00:13 +0300

    Update EmployeeContract.java

--- dev/partA/EmployeeContract.java
+++ dev/partA/EmployeeContract.java
@@ -1,2 +1,12 @@
 public class EmployeeContract {
+    private Employee employee;
+    private String startDate;
+    private int freeDays;
+    private int sicknessDays;
+    private int monthlyWorkHours;
+    private String socialContributions;
+    private String advancedStudyFund;
+    private int salary;
+    private String archivedAt;
+    private boolean isArchived;
 }
