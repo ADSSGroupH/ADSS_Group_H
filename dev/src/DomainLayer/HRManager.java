9541111ae72441615f092b57a39e4f3f4f41125a
yardsch <yardsc@post.bgu.ac.il>
2025-04-17 18:32:23 +0300

    Add files via upload

---
+++ dev/src/DomainLayer/HRManager.java
@@ -0,0 +1,11 @@
+package DomainLayer;
+
+import java.util.Set;
+
+public class HRManager extends Employee {
+    public HRManager(String id, String name, String phoneNumber, Branch branch,
+                     Set<Role> roles, EmployeeContract contract, String bankDetails,
+                     boolean isArchived, String archivedAt, String password) {
+        super(id, name, phoneNumber, branch, roles, contract, bankDetails, isArchived, archivedAt, true, password);
+    }
+}
