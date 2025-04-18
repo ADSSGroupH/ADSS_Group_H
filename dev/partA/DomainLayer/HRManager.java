f1f2de1776c7c3b42c5cd20a8d3933f7474287b8
yardsch <yardsc@post.bgu.ac.il>
2025-04-18 18:51:57 +0300

    Update HRManager.java

--- dev/partA/DomainLayer/HRManager.java
+++ dev/partA/DomainLayer/HRManager.java
@@ -1,9 +1,10 @@
+import java.time.LocalDate;
 import java.util.Set;
 
 public class HRManager extends Employee {
     public HRManager(String id, String name, String phoneNumber, Branch branch,
                      Set<Role> roles, EmployeeContract contract, String bankDetails,
-                     boolean isArchived, String archivedAt, String password) {
+                     boolean isArchived, LocalDate archivedAt, String password) {
         super(id, name, phoneNumber, branch, roles, contract, bankDetails, isArchived, archivedAt, true, password);
     }
 }
