71d370dc4ed7a882fd47716c7127832ac4fe0fa6
yardsch <yardsc@post.bgu.ac.il>
2025-04-18 18:55:00 +0300

    Update ShiftAssignment.java

--- dev/partA/DomainLayer/ShiftAssignment.java
+++ dev/partA/DomainLayer/ShiftAssignment.java
@@ -1,12 +1,17 @@
+import java.time.LocalDate;
+import java.util.Date;
+
 public class ShiftAssignment {
     private Employee employee;
     private Shift shift;
     private Role role;
+    private LocalDate ArchiveDate;
 
-    public ShiftAssignment(Employee employee, Shift shift, Role role) {
+    public ShiftAssignment(Employee employee, Shift shift, Role role, LocalDate ArchiveDate) {
         this.employee = employee;
         this.shift = shift;
         this.role = role;
+        this.ArchiveDate = ArchiveDate;
     }
 
     public Employee getEmployee() {
@@ -20,6 +25,9 @@ public class ShiftAssignment {
     public Role getRole() {
         return role;
     }
+    public LocalDate getArchiveDate() {
+        return ArchiveDate;
+    }
 
     public void setEmployee(Employee employee) {
         this.employee = employee;
@@ -31,5 +39,8 @@ public class ShiftAssignment {
 
     public void setRole(Role role) {
         this.role = role;
+    }
+    public void setArchiveDate(LocalDate ArchiveDate) {
+        this.ArchiveDate = ArchiveDate;
     }
 }
