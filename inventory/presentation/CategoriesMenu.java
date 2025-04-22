bec56f7bfaf4fef3fb4126e0f0dc926fe246aef7
AlmogSaa <almogsaa@post.bgu.ac.il>
2025-04-22 15:31:11 +0300

    Add files via upload

---
+++ inventory/presentation/CategoriesMenu.java
@@ -0,0 +1,8 @@
+package inventory.presentation;
+
+import inventory.view.CategoriesView;
+
+class CategoriesMenu {
+    private CategoriesView categoriesList = new CategoriesView();
+    public CategoriesView getCategoriesList() { return categoriesList; }
+}
---
+++ inventory/presentation/ReportsMenu.java
@@ -0,0 +1,8 @@
+package inventory.presentation;
+
+import inventory.view.ReportsView;
+
+class ReportsMenu {
+    private ReportsView reportsList = new ReportsView();
+    public ReportsView getReportsList() { return reportsList; }
+}
