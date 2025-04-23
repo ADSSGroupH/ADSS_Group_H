9c5d30963b828741e7c19d04b62cb69512a46abf
Shay-Silversmith <ah20002@gmail.com>
2025-04-23 19:28:27 +0300

    Update Alert.java

--- inventory/domain/Alert.java
+++ inventory/domain/Alert.java
@@ -12,4 +12,26 @@ public class Alert {
         this.date    = date;
         this.pid    = pid;
     }
+
+    
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
 }
