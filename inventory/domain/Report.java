aa253c45cbb746dd431485994a38161751e5ead0
Shay-Silversmith <ah20002@gmail.com>
2025-04-23 19:36:05 +0300

    Update Report.java

--- inventory/domain/Report.java
+++ inventory/domain/Report.java
@@ -10,12 +10,41 @@ public abstract class Report {
     private List<Product> products;
     private List<Category> categories;
 
-    public Report(String reportID, Date date,
+    public Report(String reportID,
+                  Date date,
                   List<Product> products,
                   List<Category> categories) {
         this.reportID   = reportID;
         this.date       = date;
         this.products   = products;
+        this.categories = categories;
+    }
+
+    public String getReportID() {
+        return reportID;
+    }
+    public void setReportID(String reportID) {
+        this.reportID = reportID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+    public void setCategories(List<Category> categories) {
         this.categories = categories;
     }
 }
