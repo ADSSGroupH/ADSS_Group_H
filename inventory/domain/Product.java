3db1ebb6f987926b91d446f27f7d9693f305824b
Shay-Silversmith <ah20002@gmail.com>
2025-04-23 19:26:32 +0300

    Update Product.java

--- inventory/domain/Product.java
+++ inventory/domain/Product.java
@@ -46,4 +46,102 @@ public class Product {
         this.category            = category;
     }
     // … getters & setters …
+    
+    public String getPid() {
+        return pid;
+    }
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getCostPrice() {
+        return costPrice;
+    }
+    public void setCostPrice(double costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+    public void setSalePrice(double salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public List<Promotion> getPromotions() {
+        return promotions;
+    }
+    public void setPromotions(List<Promotion> promotions) {
+        this.promotions = promotions;
+    }
+
+    public List<SupplierDiscount> getSupplierDiscounts() {
+        return supplierDiscounts;
+    }
+    public void setSupplierDiscounts(List<SupplierDiscount> supplierDiscounts) {
+        this.supplierDiscounts = supplierDiscounts;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public int getMinQuantity() {
+        return minQuantity;
+    }
+    public void setMinQuantity(int minQuantity) {
+        this.minQuantity = minQuantity;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public int getWarehouseQuantity() {
+        return warehouseQuantity;
+    }
+    public void setWarehouseQuantity(int warehouseQuantity) {
+        this.warehouseQuantity = warehouseQuantity;
+    }
+
+    public int getShelfQuantity() {
+        return shelfQuantity;
+    }
+    public void setShelfQuantity(int shelfQuantity) {
+        this.shelfQuantity = shelfQuantity;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
