7eb6d4375670ae421356da60874831e986ffc4f5
Shay-Silversmith <ah20002@gmail.com>
2025-04-23 19:36:54 +0300

    Update SupplierDiscount.java

--- inventory/domain/SupplierDiscount.java
+++ inventory/domain/SupplierDiscount.java
@@ -20,4 +20,39 @@ public class SupplierDiscount {
         this.startDate           = startDate;
         this.endDate             = endDate;
     }
+
+    public String getDisID() {
+        return disID;
+    }
+    public void setDisID(String disID) {
+        this.disID = disID;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
