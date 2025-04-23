936aa7588536990672bf9b54ed5186f3962fe902
TalYitzhaky <yitzhaky@post.bgu.ac.il>
2025-04-23 19:41:29 +0300

    fixes to user controller

--- dev/domain_layer/UserController.java
+++ dev/domain_layer/UserController.java
@@ -12,13 +12,20 @@ public class UserController {
         users.add(new User( "Tal","123", User.Role.SystemManager));
     }
     public String addUser(String userName, String password, User.Role role) {
-        int id = users.size() + 1;
+        for (User user : users) {
+            if (user.getUserName().equals(userName)) {
+                return "User already exists";
+            }
+        }
         User user = new User(userName, password, role);
         users.add(user);
         return "User added successfully";
     }
 
     public String login(String userName, String password) {
+        if (currentUser != null) {
+            return "User already logged in";
+        }
         for (User user : users) {
             if (user.getUserName().equals(userName) && user.getPassword().equals(password)) {
                 currentUser = user;
@@ -38,6 +45,13 @@ public class UserController {
             }
         }
         return "User not found";
+    }
+    public String logout() {
+        if (currentUser != null) {
+            currentUser = null;
+            return "Logout successful";
+        }
+        return "No user is currently logged in";
     }
 
     
