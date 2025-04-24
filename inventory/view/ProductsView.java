601f46d2419dcb5ba7b55ce1adf517c68022461a
AlmogSaa <almogsaa@post.bgu.ac.il>
2025-04-24 18:27:04 +0300

    Update ProductsView.java

--- inventory/view/ProductsView.java
+++ inventory/view/ProductsView.java
@@ -5,8 +5,14 @@ import java.util.List;
 
 public class ProductsView {
     private List<Product> products;
-    public void setProducts(List<Product> products) {
+    public ProductsView(List<Product> products) {
         this.products = products;
     }
-    public List<Product> getProducts() { return products; }
+    public void display() {
+        System.out.println("=== Products List ===");
+        for (Product p : products) {
+            System.out.printf("ID: %s | Name: %s | Stock: %d\n",
+                    p.getPid(), p.getName(), p.getStockQuantity());
+        }
+    }
 }
