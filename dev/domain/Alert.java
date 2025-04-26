edb4e8b6ed3a58eccbd30ee25087e991721f1185
AlmogSaa <almogsaa@post.bgu.ac.il>
2025-04-26 23:38:19 +0300

    Create Alert.java

---
+++ dev/domain/Alert.java
@@ -0,0 +1,43 @@
+package inventory.domain;
+
+import java.util.Date;
+
+public class Alert {
+    private String message;
+    private Product product;
+    private Date date;
+    private String pid;
+
+    public Alert(Product product, Date date) {
+        this.product = product;
+        this.date    = date;
+        this.pid    = product.getPid();
+        this.message = "\nShortage alert:\n" + product.getName() +
+                " (ID: " + product.getPid() + ") is low in stock.\n" +
+                "Minimum quantity:" + product.getMinQuantity() +"\nStock quantity:" + product.getStockQuantity();
+    }
+    public Product getProduct() { return product; }
+
+    public void printShortageMessage() {
+        System.out.println(this.message);
+    }
+
+    @Override
+    public String toString() {
+        return this.message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+}
