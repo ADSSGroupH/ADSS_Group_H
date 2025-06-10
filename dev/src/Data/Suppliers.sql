752a3a20324b10fafb4184882f8d58f9b71ec72d
Shay-Silversmith <ah20002@gmail.com>
2025-06-10 19:09:26 +0300

    Delete dev/src/Data directory

--- dev/src/Data/Suppliers.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "Suppliers" VALUES ('SUP1', 'Roger', '111', 'CHECK', '12 HaSadot St.');
-INSERT INTO "Suppliers" VALUES ('SUP2', 'Strauss', '222', 'CREDIT_CARD', '45 Milkman Rd., Haifa');
-INSERT INTO "Suppliers" VALUES ('SUP3', 'Osem', '333', 'CASH', '78 Dough Ave., Jerusalem');
-INSERT INTO "Suppliers" VALUES ('1111111', 'd', '3', 'CASH', 's');
-INSERT INTO "Suppliers" VALUES ('sup19', 'tal', 'jlfs', 'CHECK', 'hdhs');
