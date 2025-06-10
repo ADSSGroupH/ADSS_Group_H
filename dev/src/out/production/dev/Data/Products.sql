91bf9ffe0764335caf6df67adcc4b8467dd6e0bb
Shay-Silversmith <ah20002@gmail.com>
2025-06-10 18:20:49 +0300

    Add files via upload

---
+++ dev/src/out/production/dev/Data/Products.sql
@@ -0,0 +1,3 @@
+INSERT INTO "Products" VALUES ('P1', 'Milk', 2.0, 3.5, 'Tnuva', NULL, NULL, 20)
+INSERT INTO "Products" VALUES ('P2', 'Bread', 1.0, 2.5, 'Angel', NULL, NULL, 15);
+INSERT INTO "Products" VALUES ('P3','Shampoo',10.0,15.0,'Head&Shoulders',0);
