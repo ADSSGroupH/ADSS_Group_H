eaac4f19e60234a6007ae68b5d12fb8049eb582d
yardsch <yardsc@post.bgu.ac.il>
2025-06-14 13:17:42 +0300

    Delete src directory

--- src/main/java/DomainLayer/HR/Enums/ShiftSwapRequest_Status.java
+++
@@ -1,9 +0,0 @@
-package DomainLayer.HR.Enums;
-
-public enum ShiftSwapRequest_Status {
-    RequestNotApproved,
-    RequesterIsntAssignedToOgShift,
-    RequesterIsNotQualifiedForNewShift,
-    OtherIsNotQualifiedForRequesterRole,
-    RequestApproved;
-}
